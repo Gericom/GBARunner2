3ea230d42bc6973f107ffb836930c038c867cbe8
Gericom <fnouwt2@gmail.com>
2020-02-16 16:54:49 +0100

    Set swi protected bios op after swi execution instead of before

--- arm9/source/bios.s
+++ arm9/source/bios.s
@@ -5,16 +5,9 @@ gGbaBios:
 
 .global bios_swiVeneer
 bios_swiVeneer:
-	b swiPatch
+	b (gGbaBios + 0x140)
+	//b swiPatch
 
 .space 0x4000 - (. - gGbaBios)
-
-swiPatch:
-	push {r0,r1}
-	ldr r0,= gBiosOp
-	ldr r1,= 0xE3A02004
-	str r1, [r0]
-	pop {r0, r1}
-	b (gGbaBios + 0x140)
 
 .pool
