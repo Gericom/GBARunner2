Gericom/GBARunner2

commit f3daa51d504877e92c124be533706562f8f30a55

Fixed 32 bit cascade timers used as counter with base timer having reload value 0

arm9/source/dtcm_data.s
@@ -303,6 +303,9 @@ dma_shadow_regs_dtcm:
 	.short 0 //control
 .endr
 
+//dummy reload of fake timer before 0
+.short 0
+
 .global timer_shadow_regs_dtcm
 timer_shadow_regs_dtcm:
 .rept 4

arm9/source/emu/timer.s
@@ -8,25 +8,22 @@
 
 ADDRESS_TIMER_BASE = 0x04000100
 
-// .macro fix_timer_counter_read regb, rega
-// 	mov \regb, \rega, lsl #16
-// 	tst \rega, #(4 << 16)
-// 	movne \regb, \regb, lsr #16 //if slave mode, don't divide by 2
-// 	moveq \regb, \regb, lsr #17 //if normal mode, fix timer value
-// 	orreq \regb, \regb, #0x8000 //set top bit as we assume this bit was 1 (we don't really have a proper way of fixing that)
-// .endm
-
-.macro fix_timer_counter_read regb, rega, regc
+.macro fix_timer_counter_read regb, rega, regc, regd
 	tst \rega, #(4 << 16)
-	mov \regb, \rega, lsl #16
-		movne \regb, \regb, lsr #16 //if slave mode, don't divide by 2
 		bne 1f
+	mov \regb, \rega, lsl #16
 	mov \regb, \regb, lsr #17 //if normal mode, fix timer value
 	tst \regc, #0x8000
 		orrne \regb, #0x8000
 	cmp \regb, \regc
 		eorlt \regb, #0x8000
+	b 2f
 1:
+	cmp \regd, #0	
+	mov \regb, \rega, lsl #16
+		movne \regb, \regb, lsr #16 //if slave mode, and reload of parent timer is not 0, don't divide by 2
+		moveq \regb, \regb, lsr #17
+2:
 .endm
 
 .global read_address_timer_counter
@@ -38,36 +35,30 @@ read_address_timer_counter:
 	sub r12, r9, r12
 	add r13, r12, lsr #1
 	ldrh r11, [r13]
+	ldrh r12, [r13, #-10] //reload of parent timer
 
-	fix_timer_counter_read r10, r10, r11
+	fix_timer_counter_read r10, r10, r11, r12
 
 	strh r10, [r13]
-
-	//ldrh r10, [r9]
-	//mov r10, r10, lsr #1
 	bx lr
 
 .global read_address_timer
 read_address_timer:
 	ldr r10, [r9]
-
 	
 	ldr r12,= ADDRESS_TIMER_BASE
 	ldr r13,= timer_shadow_regs_count_dtcm
 	sub r12, r9, r12
 	add r13, r12, lsr #1
 	ldrh r11, [r13]
+	ldrh r12, [r13, #-10] //reload of parent timer
 
-	fix_timer_counter_read r12, r10, r11
+	fix_timer_counter_read r12, r10, r11, r12
 
 	strh r12, [r13]
 
 	mov r10, r10, lsr #16
 	orr r10, r12, r10, lsl #16
-	//mov r12, r10, lsr #16
-	//mov r12, r12, lsl #16
-	//mov r13, r10, lsl #16
-	//orr r10, r12, r13, lsr #17
 	bx lr
 
 .global write_address_timer_counter
@@ -123,13 +114,13 @@ write_address_timer_counter:
 .global write_address_timer_control
 write_address_timer_control:
 	tst r11, #0x80
-	streqh r11, [r9] //if the timer is not set to run, we don't have to fix anything
-	beq 1f
+		streqh r11, [r9] //if the timer is not set to run, we don't have to fix anything
+		beq 1f
 
 	ldrh r12, [r9]
 	tst r12, #0x80
-	strneh r11, [r9] //if the timer was already running, we don't have to fix anything
-	bne 1f
+		strneh r11, [r9] //if the timer was already running, we don't have to fix anything
+		bne 1f
 
 	ldr r13,= timer_shadow_regs_dtcm
 	ldr r12,= (ADDRESS_TIMER_BASE + 2)
@@ -162,14 +153,14 @@ write_address_timer_control:
 	ldr r12,= dsp_sendIpcCommand
 	blx r12
 	cmp r0, #0
-	beq 2b //loop if it failed
+		beq 2b //loop if it failed
 	pop {r0-r3,lr}
 #else
 	ldr r13,= 0x04000188
 2:
 	ldr r10, [r13, #-4]
 	tst r10, #1
-	beq 2b
+		beq 2b
 	ldr r12,= 0x04000188
 	str r9, [r12]
 	str r11, [r12]
@@ -186,8 +177,8 @@ write_address_timer:
 	strh r11, [r13]
 
 	tst r11, #(4 << 16) //if slave mode, we don't have to fix the reload value
-	strne r11, [r9]
-	bne 1f
+		strne r11, [r9]
+		bne 1f
 #ifdef USE_ACTUAL_RATE
 	ldr r13,= 16756991
 	smulwb r12, r13, r11
@@ -213,14 +204,14 @@ write_address_timer:
 	ldr r12,= dsp_sendIpcCommand
 	blx r12
 	cmp r0, #0
-	beq 2b //loop if it failed
+		beq 2b //loop if it failed
 	pop {r0-r3,lr}
 #else
 	ldr r13,= 0x04000188
 2:
 	ldr r10, [r13, #-4]
 	tst r10, #1
-	beq 2b
+		beq 2b
 	ldr r12,= 0x04000188
 	str r9, [r12]
 	str r11, [r12]

commit 3d352b5b988e51801972b5e10a25c9e4077eb1c1

Fix wrong alignment in dtcm introduced by f3daa51

arm9/source/dtcm_data.s
@@ -318,6 +318,8 @@ timer_shadow_regs_count_dtcm:
 	.short 0 //counter value
 .endr
 
+.align 2
+
 //the current bios opcode that is returned if you do a protected read
 //[00DCh+8] = 0xE129F000, after startup and softreset //before this address 0x27C is read
 //[0134h+8] = 0xE25EF004, during irq execution